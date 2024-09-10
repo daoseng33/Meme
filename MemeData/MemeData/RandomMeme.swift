ce089b014bd09b38eacbd01b18438eba1a4891ad
DAO <daoseng33@gmail.com>
2024-09-10 16:14:24 +0800

    chore: decode url as URL? type

--- MemeData/MemeData/RandomMeme.swift
+++ MemeData/MemeData/RandomMeme.swift
@@ -10,6 +10,21 @@ import Foundation
 public struct RandomMeme: Decodable {
     public let id: Int
     public let description: String
-    public let url: String
+    public let url: URL?
     public let type: String
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        id = try container.decode(Int.self, forKey: .id)
+        description = try container.decode(String.self, forKey: .description)
+        type = try container.decode(String.self, forKey: .type)
+
+        let urlString = try container.decode(String.self, forKey: .url)
+        self.url = URL(string: urlString)
+    }
+
+    private enum CodingKeys: String, CodingKey {
+        case id, description, url, type
+    }
 }
