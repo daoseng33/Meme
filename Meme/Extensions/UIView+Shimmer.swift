1d83f9b5b48cd6e3deb8373db7c3ac2a1f539a47
DAO <daoseng33@gmail.com>
2024-09-16 14:13:46 +0800

    choere: sort files

---
+++ Meme/Extensions/UIView+Shimmer.swift
@@ -0,0 +1,12 @@
+//
+//  UIView+Shimmer.swift
+//  Meme
+//
+//  Created by DAO on 2024/9/16.
+//
+
+import UIKit
+import UIView_Shimmer
+
+extension UIImageView: ShimmeringViewProtocol { }
+extension UITextView: ShimmeringViewProtocol { }
