3b80cf01093ffbda88abefbdb7483f641cff73d6
DAO <daoseng33@gmail.com>
2024-09-18 14:18:28 +0800

    feat: creat random joke files

---
+++ Meme/Scenes/Home/RandomJoke/RandomJokeViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  RandomJokeViewModel.swift
+//  Meme
+//
+//  Created by DAO on 2024/9/18.
+//
+
+import Foundation
---
+++ Meme/Scenes/Home/RandomJoke/RandomJokeViewModelProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  RandomJokeViewModelProtocol.swift
+//  Meme
+//
+//  Created by DAO on 2024/9/18.
+//
+
+import Foundation
