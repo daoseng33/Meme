73a70e50e5ab7c960f750c0827216359d2b43810
DAO <daoseng33@gmail.com>
2024-09-20 16:14:02 +0800

    chore: add load first data tests

--- MemeTests/RandomJokeTests.swift
+++ MemeTests/RandomJokeTests.swift
@@ -17,6 +17,19 @@ struct RandomJokeTests {
         sut = RandomJokeViewModel(webService: mockWebService)
     }
     
+    @Test func testLoadFirstDataIfNeeded() async throws {
+        assert(sut.loadingState == .initial)
+        
+        let initialJoke = try sut.joke.take(1).toBlocking().last()
+        assert(initialJoke == "")
+        
+        sut.loadFirstMemeIfNeeded()
+        assert(sut.loadingState == .success)
+        
+        let loadedJoke = try sut.joke.take(1).toBlocking().last()
+        assert(loadedJoke == "Can you swim? Some times. What do you mean by \"some times\"? Only when I'm in the water.")
+    }
+    
     @Test func testFetchRandomJoke() async throws {
         assert(sut.loadingState == .initial)
         
@@ -35,6 +48,7 @@ struct RandomJokeTests {
     }
     
     @Test func testSelectedJokeCategory() async throws {
+        // initial value
         assert(sut.selectedCategory == .Random)
         
         sut.selectedCategoryObserver.onNext("Clean")
