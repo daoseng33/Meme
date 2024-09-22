7a09e941706fe9ab0991d740420728b2fc05fe4a
DAO <daoseng33@gmail.com>
2024-09-22 14:05:47 +0800

    chore: add gifs web api

---
+++ WebAPI/WebAPI/APIService/GIFs/GIFsAPI.swift
@@ -0,0 +1,54 @@
+//
+//  GIFsAPI.swift
+//  WebAPI
+//
+//  Created by DAO on 2024/9/22.
+//
+
+import Foundation
+import Moya
+
+enum GIFsAPI {
+    case searchGiFs(query: String, number: Int)
+}
+
+extension GIFsAPI: MemeTargetType {
+    var path: String {
+        switch self {
+        case .searchGiFs:
+            return "/gif/search"
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .searchGiFs:
+            return .get
+        }
+    }
+    
+    var task: Moya.Task {
+        switch self {
+        case .searchGiFs(let query, let number):
+            let parameters: [String: Any] = [
+                "query": query,
+                "number": number
+            ]
+            return .requestParameters(parameters: parameters, encoding: URLEncoding.default)
+        }
+    }
+    
+    var headers: [String : String]? {
+        switch self {
+        case .searchGiFs:
+            return nil
+        }
+    }
+    
+    var sampleData: Data {
+        switch self {
+        case .searchGiFs:
+            return Utility.loadJSON(filename: "search_gifs")
+        }
+    }
+}
